f0073a27591462ff10771853ea45958f4751cb08
unknown <reallywhoknows>
2024-03-10 21:15:15 -0500

    Updated extensions name

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ prefix = "!"
 intents = discord.Intents.default()
 intents.members = True
 intents.messages = True
+intents.message_content = True
 
 client = commands.Bot(command_prefix=prefix, intents=intents)
 
