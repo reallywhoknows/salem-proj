001dd16387b387da007855f4e07ec234bba5924a
unknown <reallywhoknows>
2024-03-16 09:58:34 -0500

    Improper indent, causing all messages to create tickets LOL...

--- extensions/ticket_handler.py
+++ extensions/ticket_handler.py
@@ -41,51 +41,51 @@ class listener(commands.Cog):
         if isinstance(message.channel,discord.DMChannel):
             await message.channel.send("Thank you for your report, a ticket will be generated momentarily. Please refer back to the Salem discord.")
 
-        # Establish connection to db
-        connection = sqlite3.connect("db//tickets.db")
-        cursor = connection.cursor()
+            # Establish connection to db
+            connection = sqlite3.connect("db//tickets.db")
+            cursor = connection.cursor()
 
-        # Generate table
-        connection.execute('''CREATE TABLE IF NOT EXISTS tickets (rowid INTEGER PRIMARY KEY, user_id TEXT, thread_id TEXT)''')
+            # Generate table
+            connection.execute('''CREATE TABLE IF NOT EXISTS tickets (rowid INTEGER PRIMARY KEY, user_id TEXT, thread_id TEXT)''')
 
-        # Working variables
-        guild = self.client.get_guild(1087515364017066135)
-        channel = discord.utils.get(guild.channels, name="tickets")
-        user = message.author
-        open_ticket = None
+            # Working variables
+            guild = self.client.get_guild(1087515364017066135)
+            channel = discord.utils.get(guild.channels, name="tickets")
+            user = message.author
+            open_ticket = None
 
-        # Queries DB looking for if the user has ever made at ticket
-        res = cursor.execute(f'''SELECT user_id FROM tickets WHERE user_id="{user.id}"''')
+            # Queries DB looking for if the user has ever made at ticket
+            res = cursor.execute(f'''SELECT user_id FROM tickets WHERE user_id="{user.id}"''')
 
-        # Creates ticket for first time ticket creators
-        if res.fetchall() == []:
-            open_ticket = await listener.generate_ticket(user, connection, cursor, channel)
-        else:
-            #Grabs any tickets by thread id if the user has created one before
-            res = cursor.execute(f'''SELECT thread_id FROM tickets WHERE user_id="{user.id}"''')
-            for r in res.fetchall():
-                thread_id = int(r[0])
-                thread = channel.get_thread(thread_id)
-            
-            # Checks if any of the tickets the user previously created are open
-            if not thread.locked:
-                open_ticket = thread
+            # Creates ticket for first time ticket creators
+            if res.fetchall() == []:
+                open_ticket = await listener.generate_ticket(user, connection, cursor, channel)
+            else:
+                #Grabs any tickets by thread id if the user has created one before
+                res = cursor.execute(f'''SELECT thread_id FROM tickets WHERE user_id="{user.id}"''')
+                for r in res.fetchall():
+                    thread_id = int(r[0])
+                    thread = channel.get_thread(thread_id)
                 
-        # Creates new ticket if all other tickets are closed.
-        if open_ticket == None:
-            open_ticket = await listener.generate_ticket(user, connection, cursor, channel)
+                # Checks if any of the tickets the user previously created are open
+                if not thread.locked:
+                    open_ticket = thread
+                    
+            # Creates new ticket if all other tickets are closed.
+            if open_ticket == None:
+                open_ticket = await listener.generate_ticket(user, connection, cursor, channel)
 
-        # Content handler
-        # Check if message is empty & if there are attachments
-        if message.content == "":
+            # Content handler
+            # Check if message is empty & if there are attachments
+            if message.content == "":
+                for i in message.attachments:
+                    await open_ticket.send(i.url)
+                    return
+
+            # Sends message content and attachments if there are any
+            await open_ticket.send(f"{message.content}")
             for i in message.attachments:
                 await open_ticket.send(i.url)
-                return
-
-        # Sends message content and attachments if there are any
-        await open_ticket.send(f"{message.content}")
-        for i in message.attachments:
-            await open_ticket.send(i.url)
 
 
 
